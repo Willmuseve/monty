7828655b6725260bf50373707bbe2105353bb84a
Will Museve <willmuseve@gmail.com>
2023-03-23 22:28:33 -0700

    pop

---
+++ pop.c
@@ -0,0 +1,23 @@
+#include "monty.h"
+
+/**
+ * pop - removes the top element of the stack.
+ * @stack: the stack
+ * @line_number: the current line number
+ *
+ * Return: void
+ */
+void pop(stack_t **stack, unsigned int line_number)
+{
+	stack_t *t;
+
+	if ((*stack) == NULL)
+	{
+		fprintf(stdout, "L%d: can't pop an empty stack\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	t = *stack;
+	*stack = (*stack)->next;
+	free(t);
+}
